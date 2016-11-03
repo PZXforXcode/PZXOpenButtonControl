e3f198614117ac699a3d9d23eeb0ac9dfe71a742
彭祖鑫 <496912046@qq.com>
2016-11-03 15:41:34 +0800

    Initial Commit

---
+++ PZXOpenButtonControl/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PZXOpenButtonControl
+//
+//  Created by pzx on 16/11/3.
+//  Copyright © 2016年 pzx. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
